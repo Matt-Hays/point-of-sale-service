32a12d5d2dd28c3eeeb2a6acf5837eb6336cd638
MattHays2 <mhays@distorted.dev>
2025-02-14 14:10:41 -0700

    Added Spring Boot skeleton

---
+++ src/main/java/com/courseproject/pointofsaleservice/PointofsaleserviceApplication.java
@@ -0,0 +1,13 @@
+package com.courseproject.pointofsaleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointofsaleserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointofsaleserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/courseproject/pointofsaleservice/PointofsaleserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseproject.pointofsaleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointofsaleserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
